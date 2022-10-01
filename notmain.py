592efba27f7934d2974d8bf02b3351a1829071b6
Elijah Derwahl <78825133+Suollo@users.noreply.github.com>
2022-10-01 03:33:52 +0000

    This is the YEETUS (Yes Eli Extended The Usable System) update: adding an entire new gamemode (probably still buggy but we'll get to that later)

---
+++ notmain.py
@@ -0,0 +1,9 @@
+gamemode = input("What gamemode would you like to play  1 = set score mode 2 = set amount mode")
+tryparse and all that jazz
+if gamemode == 1:
+  while score != userinputtedscore:
+  all the code to do questions
+  tries = tries + 1
+print("congratulations you have achieved your goal of " + userinputtedscore + " in " + tries + "tries")
+else:
+  all the shizz to do gm no1
